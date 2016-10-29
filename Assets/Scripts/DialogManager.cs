9f0a1c2cd2d2d5a7621cafb9313eb15b2b2d1e01
Astoration <ybdh00@naver.com>
2016-10-30 05:31:28 +0900

    modify comment text

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -7,16 +7,26 @@ public class DialogManager : MonoBehaviour {
 	public Text Dialog;
     public AudioClip typing;    //사운드 파일을 가진다
     public static DialogManager instance;//사운드매니저의 위치를 담는다
-
+	public bool nowTyping = false;
+	public TextAsset comment;
+	private int i = 0;
+	public string[] texts = new string[100];
+	public bool isBreaking = false;
     AudioSource audio;
     IEnumerator textReading(string text){
+		nowTyping = true;
 		string stringStack = "";
+		isBreaking = false;
 		for (int i = 0; i < text.Length+1; i++) {
+			if (isBreaking)
+				i = text.Length;
 			stringStack = text.Substring (0, i);
 			Dialog.text = stringStack;
             audio.PlayOneShot(typing);
 			yield return new WaitForSeconds (0.1f);
 		}
+		isBreaking = false;
+		nowTyping = false;
 	}
     void Awake()
     {
@@ -28,11 +38,15 @@ public class DialogManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         audio = GetComponent<AudioSource>();// 오디오 컴퍼넌스를 담는다.
-        StartCoroutine (textReading ("마하라반야밀다심경관세음보살"));
+		texts = comment.text.Split('\n');
+		StartCoroutine (textReading (texts [i++]));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!nowTyping && Input.GetKeyDown (KeyCode.Return) && i < texts.Length)
+			StartCoroutine (textReading (texts [i++]));
+		else if (Input.GetKeyDown (KeyCode.Return))
+			isBreaking = true;
 	}
 }
