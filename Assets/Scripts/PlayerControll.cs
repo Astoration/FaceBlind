249ff03212ba8c47c179b5d3c92d08f36614bf3c
Astoration <ybdh00@naver.com>
2016-10-29 15:51:59 +0900

    add character animation

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -6,10 +6,12 @@ public class PlayerControll : MonoBehaviour {
 	float jumpPower = 1250f;
 	private Rigidbody2D rigid;
 	private SpriteRenderer sprite;
+	private Animator animationControl;
 	// Use this for initialization
 	void Start () {
 		rigid = GetComponent<Rigidbody2D> ();
 		sprite = GetComponent<SpriteRenderer> ();
+		animationControl = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -19,14 +21,18 @@ public class PlayerControll : MonoBehaviour {
 		
 	void moveControll(){
 		float deltaX = Input.GetAxis ("Horizontal");
-		float deltaY = Input.GetAxis ("Vertical");
 		transform.Translate (Vector2.right* deltaX * speed * Time.deltaTime);
 		if (deltaX < 0) {
 			sprite.flipX = true;
 		} else if (deltaX > 0) {
 			sprite.flipX = false;
 		}
-		if (Input.GetKeyDown(KeyCode.Space)&&rigid.velocity.y==0) {
+		if (deltaX == 0) {
+			animationControl.SetBool ("isMovement", false);
+		} else {
+			animationControl.SetBool ("isMovement", true);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)&&Mathf.Abs(rigid.velocity.y)<0.4) {
 			rigid.AddForce (Vector2.up * jumpPower);
 		}
 	}
