ab845981ebf5539a31e62102c1035a348bf57d42
jungjune <willtriti03@naver.com>
2016-10-30 02:49:32 +0900

    Set Map

---
+++ Assets/Scripts/PotalWall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotalWall : MonoBehaviour {
+    public string scene;
+    public string direction;
+    // Use this for initialization
+    void Start () {
+	
+	}
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Days.instance.spawnDirection = direction;
+            Application.LoadLevel(scene);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
