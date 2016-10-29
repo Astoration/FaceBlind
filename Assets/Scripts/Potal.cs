40d888d5c9cfaa0cc3c47ea4a9205ad07de895ed
jungjune <willtriti03@naver.com>
2016-10-29 21:24:35 +0900

    add Potal

---
+++ Assets/Scripts/Potal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Potal : MonoBehaviour {
+    public string scene;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.DownArrow)) {
+            Application.LoadLevel(scene);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
