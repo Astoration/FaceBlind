56813afc326bffc803c0b3f503021bbdecdd77bb
Astoration <ybdh00@naver.com>
2016-10-30 03:15:49 +0900

    add InMap Portal

---
+++ Assets/Scripts/InMapPortal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InMapPortal : MonoBehaviour {
+	public GameObject portalPoint;
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.DownArrow)) {
+			col.gameObject.transform.position = portalPoint.transform.position;
+			Vector3 pos = camera.transform.position;
+			pos.x = portalPoint.transform.position.x;
+			camera.transform.position = pos;
+		}
+	}
+}
